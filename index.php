979b08adbb4f492be59c1973bffa4d6058294c97
tr3mendous <35741861+tr3mendous@users.noreply.github.com>
2023-07-09 11:27:16 +0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>How to put PHP in HTML- Date Example</title>
+</head>
+<body>
+<div>This is pure HTML message.</div>
+<div>Next, we’ll display today’s date and day by PHP!</div>
+<div>
+Today’s date is <b><?php echo date('Y/m/d') ?></b> and it’s a <b><?php echo date(‘l’) ?></b> today!
+</div>
+<div>Again, this is static HTML content.</div>
+</body>
+</html>
